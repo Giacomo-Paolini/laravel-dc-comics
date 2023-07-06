0561082975fff963c8c8af496c48a16ea856a5f4
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-07-06 20:07:21 +0200

    feat: index, show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Elenco dei Fumetti</h1>
-
-    <ul>
-        @foreach ($comics as $comic)
-            <li>{{ $comic->title }}</li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Elenco dei Fumetti</h1>
+            <div class="d-flex flex-wrap justify-content-center p-5 gap-3">
+                @foreach ($comics as $comic)
+                <div class="card col-4 p-2">
+                    <img class="card-image-top img-fluid" src="{{ $comic->thumb }}" alt="">
+                    <div class="card-body">
+                        <h5>{{ $comic->title }}</h5>
+                        <p class="card-text">{{ $comic->description }}</p>
+                        <strong>{{ $comic->price }}</strong><br>
+                        <small>{{ $comic->series }}</small><br>
+                        <button class="p-2 mt-4 bg-dark"><a href="{{route('comics.show', $comic->id)}}">Show Item</a></button>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="my-3 img-container">
+                <img class="img-fluid" src="{{ $comic->thumb }}" alt="">
+            </div>
+            <div class="p-3 d-flex flex-column justify-content-center align-items-center">
+                <h5>{{ $comic->title }}</h5>
+                <p>{{ $comic->description }}</p>
+                <strong>{{ $comic->price }}</strong><br>
+                <small>{{ $comic->series }}</small><br>
+                <button class="p-2 mt-4 bg-dark"><a href="{{route('home')}}">Back to home</a></button>
+            </div>
+        </div>
+    </div>
+@endsection
